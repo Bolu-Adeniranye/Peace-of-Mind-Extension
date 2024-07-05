d3ec73093f6125f2c34da66562c854da42dba4f0
Bolu Adeniranye <68526923+Bolu-Adeniranye@users.noreply.github.com>
2024-07-05 13:35:16 +0100

    Don't ask me what i changed

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ fetch ("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&qu
 
     document.body.style.backgroundImage = `url(${data.urls.full})`
     document.getElementById("photographer").textContent = `By: ${data.user.name}`
-	
-    
-
-    
+  
 })
     .catch((err) => {
         document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1080)`
@@ -25,15 +22,13 @@ fetch ("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&qu
     })
     .then(data => {
 
-        document.getElementById("crypto").innerHTML = `<div>
-            <img src=${data.image.small} id="bitcoinImage"/>
-    </div>
-        `
-
-                document.getElementById("crypto").innerHTML += `
-            <span>🎯: $${data.market_data.current_price.usd}<br></span>
-            <span>👆: $${data.market_data.high_24h.usd}<br></span>
-            <span>👇: $${data.market_data.low_24h.usd}<br></span>
+        document.getElementById("crypto").innerHTML = `
+                <div>
+                    <img src=${data.image.small} id="bitcoinImage"/>
+                </div>
+                <p>🎯: $${data.market_data.current_price.usd}<br></p>
+                <p>👆: $${data.market_data.high_24h.usd}<br></p>
+                <p>👇: $${data.market_data.low_24h.usd}<br></p>
         `
     })
     .catch(err => console.error(err))
